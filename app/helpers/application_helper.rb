73b1685a35e268939d5923ad4491fe4bc7e1e1c2
bmeznar <blaz.meznar.bm@gmail.com>
2020-12-07 20:11:32 +0100

    all posts bottsrap added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
   def display_navbar_options
     capture do
       if user_signed_in?
-        concat link_to "Uredi profil", edit_user_registration_path
+        concat link_to "Uredi profil", edit_user_registration_path, class: 'nav-link'
         concat " "
-        concat link_to "Odjava", destroy_user_session_path, method: :delete
+        concat link_to "Odjava", destroy_user_session_path, method: :delete, class: 'nav-link'
       else
-        concat link_to "Prijava", new_user_session_path
+        concat link_to "Prijava", new_user_session_path, class: 'nav-link'
         concat " "
-        concat link_to "Registracija", new_user_registration_path
+        concat link_to "Registracija", new_user_registration_path, class: 'nav-link'
       end
     end
   end
